cf4fdd76474f4772fb5a17132541ed3514cb87a8
otoc <otniel41apm@gmail.com>
2023-07-17 20:39:19 -0300

    v1 release

--- superappbar/test/superappbar_test.dart
+++ superappbar/test/superappbar_test.dart
@@ -1,16 +1,13 @@
-import 'package:superappbar/superappbar.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
+  //  final awesome = Awesome();
 
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+
   });
 }
